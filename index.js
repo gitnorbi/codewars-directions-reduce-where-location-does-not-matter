9b553af4f2d27586f47a8ad1fb656ea520cb4e43
norbigith <dulnorbert@gmail.com>
2021-11-18 18:24:34 +0100

    it's working

---
+++ index.js
@@ -0,0 +1,45 @@
+function dirReduc(arr){
+    let result=[];
+
+    let howManyNorthDirections=0;
+    let howManySouthDirections=0;
+    let howManyEastDirections=0;
+    let howManyWestDirections=0;
+
+    let areThereNorthOrSouth;
+    let areThereEastOrWest;
+
+    arr.forEach((value)=>{
+        howManyNorthDirections+=value=="NORTH"?1:0;
+        howManySouthDirections+=value=="SOUTH"?1:0;
+        howManyEastDirections+=value=="EAST"?1:0;
+        howManyWestDirections+=value=="WEST"?1:0;
+});
+
+    areThereNorthOrSouth=howManySouthDirections-howManyNorthDirections;
+    areThereEastOrWest=howManyWestDirections-howManyEastDirections;
+
+    if(areThereNorthOrSouth<0){
+        for(let i=0;i>areThereNorthOrSouth;i--){
+        result.push("NORTH");
+        }
+    }else if(areThereNorthOrSouth>0){
+        for(let i=0;i<areThereNorthOrSouth;i++){
+            result.push("SOUTH");
+            }
+    }
+
+    if(areThereEastOrWest<0){
+        for(let i=0;i>areThereEastOrWest;i--){
+        result.push("EAST");
+        }
+    }else if(areThereEastOrWest>0){
+        for(let i=0;i<areThereEastOrWest;i++){
+            result.push("WEST");
+            }
+    }
+    document.querySelector("#result").textContent=`The result is:${result}`;
+    return result;
+  }
+  dirReduc(["EAST","EAST","NORTH","EAST","WEST","WEST","WEST","SOUTH","SOUTH","WEST","SOUTH","SOUTH","NORTH","NORTH","EAST","EAST","NORTH","EAST"]);
+
